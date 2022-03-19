ac911888499cf231964f117c2afb7db6f5479c9f
Ryan Qian <i@bitbili.net>
2022-03-20 03:33:32 +0800

    add some checks before chroot
    Signed-off-by: Ryan Qian <i@bitbili.net>

--- chroot.sh
+++ chroot.sh
@@ -5,7 +5,22 @@ set -e
 
 . "$(dirname $(realpath $0))/preprocess.sh"
 
+# do some checks
 [[ -w ${rootfs} ]] || { echo "${rootfs} does not exists or is unwritable"; exit 1; }
+[[ -x ${rootfs}${chroot_static_qemu_bin#/} ]] || \
+  { echo "${rootfs}${chroot_static_qemu_bin#/} does not exists or is unexecutable"; exit 1; }
+interpreters="$(grep 'interpreter' /proc/sys/fs/binfmt_misc/qemu-*)"
+interpreter_matched=0
+while read -r _ interpreter; do
+  if [[ ${interpreter} == ${chroot_static_qemu_bin} ]]; then
+    interpreter_matched=1
+  fi
+done <<<"${interpreters}"
+[[ ${interpreter_matched} == 1 ]] || \
+  { echo "qemu-user binary: ${chroot_static_qemu_bin}
+is not matched with interpreters: ${interpreters}"; exit 1; }
+[[ $(cat /proc/sys/fs/binfmt_misc/status) == "enabled" ]] || { echo "binfmt disabled"; exit 1; }
+
 
 findmnt ${rootfs}tmp/ >/dev/null || mount --types tmpfs tmpfs ${rootfs}tmp/
 findmnt ${rootfs}var/tmp/ >/dev/null || mount --types tmpfs tmpfs ${rootfs}var/tmp/
