d47f561c60148d12d073a7ccf87d7caa18f6f2e1
Ryan Qian <i@bitbili.net>
2022-03-20 14:04:11 +0800

    patch portage to make build log accessible for normal users
    Signed-off-by: Ryan Qian <i@bitbili.net>

--- updateEnv.sh
+++ updateEnv.sh
@@ -10,13 +10,16 @@ echo "preparing environment ..."
 
 [[ -x ${rootfs}${chroot_static_qemu_bin#/} ]] || cp ${static_qemu_bin} ${rootfs}${chroot_static_qemu_bin#/}
 
+echo ">>> appending make.conf ..."
 makeConf="${rootfs}etc/portage/make.conf"
 sed -Ei '/^###TEST_CONF_START###$/,/^###TEST_CONF_END###$/d' ${makeConf}
 sed -E 's/@NPROC@/'$(($(nproc) - 2))'/' ${myPath}/conf.d/make.conf >>${makeConf}
 
+echo ">>> touching package.{use,unmask}/zzz ..."
 mkdir -p ${rootfs}etc/portage/package.{use,unmask}
 touch ${rootfs}etc/portage/package.{use,unmask}/zzz
 
+echo ">>> copying other configuration files ..."
 while read -r testconf; do
   file=${rootfs}etc/portage/${testconf#${myPath}/conf.d/}
   ex=0
@@ -36,5 +39,11 @@ done <<<"$(eval "find ${myPath}/conf.d \\( \
                   ! -name 'make.conf' \
                   $(for p in ${ignore_patterns[@]};do echo -n " -and ! -name '${p}'"; done) \
                   -and -type f \\)")"
+
+# patch portage to make the build dir accessible for normal users
+# this is convenient for a test environment
+echo ">>> patching permissions for portage runtime directories ..."
+sed -i 's/0o660/0o644/' ${rootfs}usr/lib/python*/site-packages/portage/util/_async/PipeLogger.py
+sed -i 's/0o700/0o755/;s/0o2770/0o2775/' ${rootfs}usr/lib/python*/site-packages/portage/package/ebuild/prepare_build_dirs.py
 
 echo "done."
