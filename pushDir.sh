768e67a0308ffcd304f014253c807e5a7b0aa1b1
Ryan Qian <i@bitbili.net>
2022-03-21 18:48:25 +0800

    pushDir: fix when no -c provided
    Signed-off-by: Ryan Qian <i@bitbili.net>

--- pushDir.sh
+++ pushDir.sh
@@ -15,47 +15,48 @@ fi
 [[ -d ${rootfs} && -x ${rootfs} ]] \
   || { echo "directory ${rootfs} does not exists or is unexecutable"; exit 1; }
 
-eval "$(grep '^[[:space:]]*PORTAGE_TMPDIR=' \
-  ${rootfs}usr/share/portage/config/make.globals \
-  ${rootfs}etc/portage/make.conf | tail -1 | cut -d':' -f2)"
+if [[ -n ${dir_pattern_seed} ]]; then
+  eval "$(grep '^[[:space:]]*PORTAGE_TMPDIR=' \
+    ${rootfs}usr/share/portage/config/make.globals \
+    ${rootfs}etc/portage/make.conf | tail -1 | cut -d':' -f2)"
 
-dirs=$(LC_ALL=C find ${rootfs}${PORTAGE_TMPDIR##/} -maxdepth 4 -type d)
+  dirs=$(LC_ALL=C find ${rootfs}${PORTAGE_TMPDIR##/} -maxdepth 4 -type d)
 
-dir_pattern=
-while read -r p; do
-  if [[ ${p} != "" ]]; then
-    dir_pattern+="\/[^\/]*${p}[^\/]*"
+  dir_pattern=
+  while read -r p; do
+    if [[ ${p} != "" ]]; then
+      dir_pattern+="\/[^\/]*${p}[^\/]*"
+    fi
+  done <<<"${dir_pattern_seed//\//$'\n'}"
+
+  declare -a select_dirs
+  while read -r p; do
+    if [[ -z ${dir_len} ]]; then
+      dir_len=${#p}
+    fi
+    if [[ ${#p} -gt ${dir_len} ]]; then
+      continue
+    fi
+    select_dirs+=("${p}")
+  done <<<$(eval "<<<'${dirs}' sed -nE '/${dir_pattern}/p'")
+
+  if [[ ${#select_dirs[@]} -gt 1 ]]; then
+    echo -e "\e[36mselect one from:\e[0m"
+    for (( i = 0; i < ${#select_dirs[@]}; i++ )); do
+      echo "  [${i}] ${select_dirs[i]}"
+    done
+    read -p "select: " theindex
+    select_dirs[0]="${select_dirs[${theindex}]}"
+  elif [[ ${#select_dirs[@]} -lt 1 || ${select_dirs[0]} == "" ]]; then
+    echo "no matched path!"
+    exit 0
   fi
-done <<<"${dir_pattern_seed//\//$'\n'}"
-
-declare -a select_dirs
-while read -r p; do
-  if [[ -z ${dir_len} ]]; then
-    dir_len=${#p}
+  if [[ -d ${select_dirs[0]} ]]; then
+    pushingdir=${select_dirs[0]}
   fi
-  if [[ ${#p} -gt ${dir_len} ]]; then
-    continue
-  fi
-  select_dirs+=("${p}")
-done <<<$(eval "<<<'${dirs}' sed -nE '/${dir_pattern}/p'")
-
-if [[ ${#select_dirs[@]} -gt 1 ]]; then
-  echo -e "\e[36mselect one from:\e[0m"
-  for (( i = 0; i < ${#select_dirs[@]}; i++ )); do
-    echo "  [${i}] ${select_dirs[i]}"
-  done
-  read -p "select: " theindex
-  select_dirs[0]="${select_dirs[${theindex}]}"
-elif [[ ${#select_dirs[@]} -lt 1 || ${select_dirs[0]} == "" ]]; then
-  echo "no matched path!"
-  exit 0
 fi
 
-if [[ -d ${select_dirs[0]} && -n ${dir_pattern_seed} ]]; then
-  pushingdir=${select_dirs[0]}
-else
-  pushingdir=${rootfs}
-fi
+: ${pushingdir:=${rootfs}}
 
 echo ">>> shell into ${pushingdir} ..."
 echo "[press CTRL-D to exit]"
